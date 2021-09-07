062dccf93d243efbafdb5ba0ee0c489174caee3a
carolinekonrad <ckonrad@live.unc.edu>
2021-09-07 09:51:22 -0400

    LS10

---
+++ lessons/LS10_challenge_question_1.py
@@ -0,0 +1,19 @@
+"""Challenge Question #1"""
+
+__author__ = "730369129"
+
+choice: int = int(input("Enter a number: "))
+
+if choice > 50:
+    if choice >75:
+        print("A")
+    else:
+        print("B")
+else:
+    if choice <25:
+        print("C")
+    else:
+        print("D")
+
+
+
