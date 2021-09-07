e9d14d1a44e34f28e37f820c94b7426530f677fd
carolinekonrad <ckonrad@live.unc.edu>
2021-09-07 09:58:29 -0400

    LS10

--- lessons/LS10.py
+++ lessons/LS10.py
@@ -4,13 +4,13 @@ __author__ = "730369129"
 
 choice: int = int(input("Enter a number: "))
 
-if choice > 50:
-    if choice >75:
+if choice < 50:
+    if choice < 25:
         print("A")
     else:
         print("B")
 else:
-    if choice <25:
+    if choice > 75:
         print("C")
     else:
         print("D")
