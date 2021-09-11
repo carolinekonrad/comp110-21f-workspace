944436a1bd8f2998d5fd206cd0cdf61ea37297ea
carolinekonrad <ckonrad@live.unc.edu>
2021-09-10 21:03:40 -0400

    EX03

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,23 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730369129"
+
+word: str = input("Enter a word: ")
+count: int = 0
+spot: int = 1
+indexing_spot: int = 0
+
+while indexing_spot < len(word) - 1:
+    while spot < len(word):
+        if word[indexing_spot] == word[spot] and indexing_spot != spot:
+            count = count + 1
+            spot = spot + 1
+        else: 
+            spot = spot + 1
+    indexing_spot = indexing_spot + 1
+    spot = 0
+
+if count > 0:
+    print(True)
+else:
+    print(False)
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,11 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730369129"
 
-# The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+integer_depth: int = int(input("Depth: "))
+number_of_checks: int = 0
+
+
+while number_of_checks < integer_depth:
+    
