7cd3c94fa8c154e2eefe19cb129d2d103c1fd55e
carolinekonrad <ckonrad@live.unc.edu>
2021-10-04 21:26:07 -0400

    EX05 Work

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,39 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730369129"
 
-# Define your functions below
+
+def only_evens(xs: list[int]) -> list[int]:
+    """Return a list containing even numbers."""
+    evens_list: list[int] = []
+    i: int = 0
+    while i < len(xs):
+        if xs[i] % 2 == 0:
+            evens_list.append(xs[i])
+        i += 1
+    return evens_list
+
+
+def sub(ys: list[int], start_index: int, end_index: int) -> list[int]:
+    """Generate a list between the indeces."""
+    i: int = 0
+    new_list: list[int] = []
+    while i < len(ys):
+        if i >= start_index and i < end_index:
+            new_list.append(ys[i])
+        i += 1
+    return new_list
+
+
+def concat(xs: list[int], ys: list[int]) -> list[int]:
+    """Concatonate two lists together."""
+    i: int = 0
+    i_2: int = 0
+    concat_list: list[int] = []
+    while i < len(xs):
+        concat_list.append(xs[i])
+        i += 1
+    while i_2 < len(ys):
+        concat_list.append(ys[i_2])
+        i_2 += 1
+    return concat_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,62 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+__author__ = "730369129"
 
-__author__ = "123456789"
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+def test_only_evens_odds() -> None:
+    """Unit test for only_evens on list of only odds."""
+    xs: list[int] = [1, 3, 5, 7, 9]
+    assert only_evens(xs) == []
+
+
+def test_only_evens_use() -> None:
+    """Unit test for only_evens on standard list."""
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    assert only_evens(xs) == [2, 4, 6]
+
+
+def test_only_evens_use_2() -> None:
+    """Unit test for only_evens on standard list."""
+    xs: list[int] = [44, 46, 45, 78, 76]
+    assert only_evens(xs) == [44, 46, 78, 76]
+
+
+def test_sub_empty() -> None:
+    """Unit test for sub on empty list."""
+    xs: list[int] = []
+    assert sub(xs, -1, 5) == []
+
+
+def test_sub_use() -> None:
+    """Unit test for sub on normal list."""
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    assert sub(xs, 2, 5) == [3, 4, 5]
+
+
+def test_sub_use_2() -> None:
+    """Unit test for sub on normal list with large second index."""
+    xs: list[int] = [2, 4, 5, 7, 9]
+    assert sub(xs, 3, 7) == [7, 9]
+
+
+def test_concat_empty() -> None:
+    """Unit test for concat on empty lists."""
+    xs: list[int] = []
+    ys: list[int] = []
+    assert concat(xs, ys) == []
+
+
+def test_concat_use() -> None:
+    """Unit test for concat on two lists."""
+    xs: list[int] = [1, 2]
+    ys: list[int] = [3, 4, 5]
+    assert concat(xs, ys) == [1, 2, 3, 4, 5]
+
+
+def test_concat_use_2() -> None:
+    """Unit test for concat on two lists."""
+    xs: list[int] = [41, 39, 7, 11]
+    ys: list[int] = [23, 14]
+    assert concat(xs, ys) == [41, 39, 7, 11, 23, 14]   
